7ae6019ca5c0f5cacd352e09e0739df2e5bab950
Moe Aoki <mt215tjk@gmail.com>
2020-08-05 15:52:35 +0900

    Yelp Restaurants and Reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning the db..."
+Restaurant.destroy_all
+puts "Creating restaurants..."
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::TvShows::RickAndMorty.location,
+    category: Restaurant::CATEGORY.sample,
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+end
+puts "Created #{Restaurant.count} restaurants..."
+
+puts "Cleaning the db..."
+Review.destroy_all
+puts "Creating reviews..."
+5.times do
+  Review.create!(
+    content: Faker::Restaurant.review,
+    rating: rand(0..5),
+    restaurant: Restaurant.all.sample
+    )
+end
+puts "Created #{Review.count} reviews..."
+
+
+
+
+
+
